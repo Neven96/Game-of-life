f2033a52dff73f1e422a8e1110276ede071dc0e8
Even <even.holmen@hotmail.no>
2023-07-24 13:29:37 +0200

    Shortened the boxed game module

--- Version 3/modules/playBoxedGame.js
+++ Version 3/modules/playBoxedGame.js
@@ -4,6 +4,28 @@ import { reDrawGame } from "./reDrawGame.js";
 import { waitGame } from "./playGame.js";
 import { aliveArrayChecker } from "./aliveArrayChecker.js";
 
+function switchHelper(row, col, rowArray, changedArray, sum) {
+    switch (sum) {
+        case 2:
+            // Stay alive/dead
+            break;
+        case 3:
+            // Stay alive/awaken
+            if (rowArray[row][col] == 0) {
+                changedArray.push([row, col]);
+            }
+            break;
+        default:
+            // Killed
+            if (rowArray[row][col] == 1) {
+                changedArray.push([row, col]);
+            }
+            break;
+    }
+
+    arrayObjects.setChangedArray = changedArray;
+}
+
 // With borders
 function playBoxedGame(changedArray) {
     let rowArray = arrayObjects.getRowArray;
@@ -19,71 +41,23 @@ function playBoxedGame(changedArray) {
             if (row == 0) {
                 // Top-left corner
                 if (col == 0) {
-                    switch (rowArray[row][col + 1]
-                          + rowArray[row + 1][col]
-                          + rowArray[row + 1][col + 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row][col + 1]
+                               + rowArray[row + 1][col]
+                               + rowArray[row + 1][col + 1]);
                 }
                 // Top-right corner
                 else if (col == rowArray[row].length - 1) {
-                    switch (rowArray[row][col - 1]
-                          + rowArray[row + 1][col]
-                          + rowArray[row + 1][col - 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row][col - 1]
+                               + rowArray[row + 1][col]
+                               + rowArray[row + 1][col - 1]);
                 }
                 // Rest of top row
                 else {
-                    switch (rowArray[row][col - 1]
-                          + rowArray[row][col + 1]
-                          + rowArray[row + 1][col - 1]
-                          + rowArray[row + 1][col]
-                          + rowArray[row + 1][col + 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row][col - 1]
+                               + rowArray[row][col + 1]
+                               + rowArray[row + 1][col - 1]
+                               + rowArray[row + 1][col]
+                               + rowArray[row + 1][col + 1]);
                 }
             } // Top row end
 
@@ -91,71 +65,23 @@ function playBoxedGame(changedArray) {
             else if (row == rowArray.length - 1) {
                 // Bottom-left corner
                 if (col == 0) {
-                    switch (rowArray[row][col + 1]
-                          + rowArray[row - 1][col]
-                          + rowArray[row - 1][col + 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row][col + 1]
+                               + rowArray[row - 1][col]
+                               + rowArray[row - 1][col + 1]);
                 }
                 // Bottom-right corner
                 else if (col == rowArray[row].length - 1) {
-                    switch (rowArray[row][col - 1]
-                          + rowArray[row - 1][col]
-                          + rowArray[row - 1][col - 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row][col - 1]
+                               + rowArray[row - 1][col]
+                               + rowArray[row - 1][col - 1]);
                 }
                 // Rest of bottom row
                 else {
-                    switch (rowArray[row][col - 1]
-                          + rowArray[row][col + 1]
-                          + rowArray[row - 1][col - 1]
-                          + rowArray[row - 1][col]
-                          + rowArray[row - 1][col + 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row][col - 1]
+                               + rowArray[row][col + 1]
+                               + rowArray[row - 1][col - 1]
+                               + rowArray[row - 1][col]
+                               + rowArray[row - 1][col + 1]);
                 }
             } // Bottom row end
 
@@ -163,84 +89,34 @@ function playBoxedGame(changedArray) {
             else {
                 // Left column
                 if (col == 0) {
-                    switch (rowArray[row - 1][col]
-                          + rowArray[row - 1][col + 1]
-                          + rowArray[row][col + 1]
-                          + rowArray[row + 1][col]
-                          + rowArray[row + 1][col + 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row - 1][col]
+                               + rowArray[row - 1][col + 1]
+                               + rowArray[row][col + 1]
+                               + rowArray[row + 1][col]
+                               + rowArray[row + 1][col + 1]);
                 }
                 // Right column
                 else if (col == rowArray[row].length - 1) {
-                    switch (rowArray[row - 1][col]
-                          + rowArray[row - 1][col - 1]
-                          + rowArray[row][col - 1]
-                          + rowArray[row + 1][col]
-                          + rowArray[row + 1][col - 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row - 1][col]
+                               + rowArray[row - 1][col - 1]
+                               + rowArray[row][col - 1]
+                               + rowArray[row + 1][col]
+                               + rowArray[row + 1][col - 1]);
                 }
                 // Middle of map
                 else {
-                    switch (rowArray[row - 1][col - 1]
-                          + rowArray[row - 1][col]
-                          + rowArray[row - 1][col + 1]
-                          + rowArray[row][col - 1]
-                          + rowArray[row][col + 1]
-                          + rowArray[row + 1][col - 1]
-                          + rowArray[row + 1][col]
-                          + rowArray[row + 1][col + 1]) {
-                        case 2:
-                            // Stay alive/dead
-                            break;
-                        case 3:
-                            // Stay alive/awaken
-                            if (rowArray[row][col] == 0) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                        default:
-                            // Killed
-                            if (rowArray[row][col] == 1) {
-                                changedArray.push([row, col]);
-                            }
-                            break;
-                    }
+                    switchHelper(row, col, rowArray, changedArray, rowArray[row - 1][col - 1]
+                               + rowArray[row - 1][col]
+                               + rowArray[row - 1][col + 1]
+                               + rowArray[row][col - 1]
+                               + rowArray[row][col + 1]
+                               + rowArray[row + 1][col - 1]
+                               + rowArray[row + 1][col]
+                               + rowArray[row + 1][col + 1]);
                 }
             }
         }
     }
-
-    arrayObjects.setChangedArray = changedArray;
 
     reDrawGame();
     aliveArrayChecker();
