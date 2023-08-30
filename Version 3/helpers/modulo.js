e8dc4ea17e1608a57f5de02105d5fac681d90845
Even <even.holmen@hotmail.no>
2023-08-30 14:30:45 +0200

    Comments on modulo helper function

--- Version 3/helpers/modulo.js
+++ Version 3/helpers/modulo.js
@@ -1,6 +1,10 @@
 import { myHeaders } from "./header.js";
 
 // Proper modulo function for JavaScript
+/**
+* @param {number} [n] "Is the number that is to be checked"
+* @param {number} [m] "Is the number that is divided by"
+*/
 function mod(n, m) {
     return ((n % m) + m) % m;
 }
